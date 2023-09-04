032bd190760d1ca1dd43c905653c5243db23a1df
hhc <1173366497@qq.com>
2023-09-04 18:57:23 +0800

    test:测试自动化部署

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-alert('hello')
+import './list.css';
+alert('hello');
+const $text=document.createElement('div');
+$text.textContent='我是李二牛';
+$text.classList.add('mys')
+document.body.appendChild($text)
