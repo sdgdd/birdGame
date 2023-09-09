6ee4bdb31725bc981dc74797f66bdfffc1088bd9
hhc <1173366497@qq.com>
2023-09-09 15:18:04 +0800

    feat:新增可定时器函数

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ require('../public/style/bird.css');
 const { Sky } = require('./sky')
 const { Land } = require('./land')
 const { Bird } = require('./bird')
-const {MangerPipe} = require('./pipe')
+const {MangerPipe} = require('./pipe');
+const {setIntervalAnimal}= require('./until')
 
 const mysky = new Sky(-10, 0);
 const land = new Land(-15);
@@ -22,24 +23,17 @@ addEventListener('click', function () {
     bird.jump()
 });
 
-let startTime = null;
 
 function animal() {
-    if (!startTime) startTime = Date.now();
-    if (Date.now() - startTime > 80) {
         startTime = Date.now()
         bird.startSwing()
         pipe.move(-0.5);
         bird.move(timeTranst(times))
         mysky.move(timeTranst(times));
         land.move(timeTranst(times));
-        
-    }
-    requestAnimationFrame(animal);
-
 }
 
-animal()
+setIntervalAnimal(animal,80)
 
 function timeTranst(time) {
     return time / 100
