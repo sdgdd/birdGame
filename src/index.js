67f4e584aa2f1ca372ae4c49f56b0094601b848d
hhc <1173366497@qq.com>
2023-09-11 15:04:21 +0800

    chore：删除冗余代码

--- src/index.js
+++ src/index.js
@@ -1,40 +1,56 @@
 require('../public/style/bird.css');
-const { Sky } = require('./sky')
-const { Land } = require('./land')
-const { Bird } = require('./bird')
-const {MangerPipe} = require('./pipe');
-const {setIntervalAnimal}= require('./until')
+// const { Sky } = require('./base/sky')
+// const { Land } = require('./base/land')
+// const { Bird } = require('./base/bird')
+// const {ProductPipePair} = require('./base/pipe');
+// const {setIntervalAnimal}= require('./until');
+// const {Scenes} = require('./control/scence')
 
-const mysky = new Sky(-10, 0);
-const land = new Land(-15);
-const bird = new Bird(0, 1);
-const pipe = new MangerPipe(10);
-pipe.genteratePipePair()
-const times = 100
+// // function init(){
+//     const mysky = new Sky(-10, 0);
+//     const land = new Land(-15);
+//     const bird = new Bird(0, 1);
+//     const pipe = new ProductPipePair(10);
+//     pipe.genteratePipePair()
+//     const times = 100;
+//     bird.startSwing()
+//     setIntervalAnimal(animal,80);
+//     const secne =new Scenes()
+// // }
+
+// document.querySelector('.mask').addEventListener('click',(e)=>{
+//     e.preventDefault();
+//     e.stopPropagation();
+//     secne.changeScene('playing');
+// })
 
 
-document.addEventListener('keydown', (event) => {
-    if (event.code === 'Space') {
-        bird.jump()
-    }
-})
+// document.addEventListener('keydown', (event) => {
+//     if (event.code === 'Space') {
+//         bird.jump()
+//     }
+// })
 
-addEventListener('click', function () {
-    bird.jump()
-});
+// addEventListener('click', function () {
+//     bird.jump()
+// });
 
 
-function animal() {
-        startTime = Date.now()
-        bird.startSwing()
-        pipe.move(-0.5);
-        bird.move(timeTranst(times))
-        mysky.move(timeTranst(times));
-        land.move(timeTranst(times));
-}
+// function animal() {
+//         pipe.move(-0.5);
+//         bird.move(timeTranst(times))
+//         mysky.move(timeTranst(times));
+//         land.move(timeTranst(times));
+// }
 
-setIntervalAnimal(animal,80)
 
-function timeTranst(time) {
-    return time / 100
-}
+
+// function timeTranst(time) {
+//     return time / 100
+// }
+
+
+const {Game} = require('./control/game');
+
+const game = new Game();
+game.init();
